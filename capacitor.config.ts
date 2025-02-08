712be33641bd6eb4cfe8f921f1865bd6b121845a
chirag96711 <chiragverma9671156188@gmail.com>
2025-02-08 12:34:42 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gogreen',
+  webDir: 'www'
+};
+
+export default config;
